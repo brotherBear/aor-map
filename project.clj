9d516ddbb95bcb1b2149555d5c486be670ddc970
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2018-04-20 21:09:14 +0200

    Added lein plugin ancient
    Signed-off-by: Bjørn Asle Taranger <bjorn.taranger@gmail.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject domain "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :javac-options ["-target" "1.9" "-source" "1.9" "-Xlint:-options"]
   :aot [domain.core]
-  :main domain.core)
+  :main domain.core
+  :plugins [[lein-ancient "0.5.5"]])
